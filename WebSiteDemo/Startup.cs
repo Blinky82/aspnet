3ae64bf388408c496d10d735d12ccfdf7cb776b5
'b_stoyanov@focus-news.net' <b_stoyanov@focus-news.net>
2017-05-18 16:37:03 +0300

    Add project files.

---
+++ WebSiteDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSiteDemo.Startup))]
+namespace WebSiteDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
